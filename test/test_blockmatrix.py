b7ec6fe53b6f96c9909dec43c567b86fc0fa4c60
Syun'ichi Shiraiwa <shiraiwa@psfc.mit.edu>
2018-01-26 17:44:59 -0500

    add more test programs

---
+++ test/test_blockmatrix.py
@@ -0,0 +1,27 @@
+import sys
+import numpy as np
+from scipy.sparse import csr_matrix
+
+if len(sys.argv) < 2:
+    import mfem.ser as mfem
+elif sys.argv[1] == 'par':
+    import mfem.par as mfem    
+
+
+smat = csr_matrix([[1,2,3,], [0, 0, 1], [2, 0, 0]])
+mmat = mfem.SparseMatrix(smat)
+
+offset1 = mfem.intArray([0, 3, 6])
+offset2 = mfem.intArray([0, 3, 6])
+
+m = mfem.BlockMatrix(offset1, offset2)
+m.SetBlock(0, 0, mmat)
+
+print m._offsets
+print m._linked_mat
+
+m = mfem.BlockMatrix(offset1)
+m.SetBlock(0, 1, mmat)
+m.SetBlock(1, 0, mmat)
+print m._offsets
+print m._linked_mat
