34eef32cd59099333eb27edcf4714dd2f63553be
Syun'ichi Shiraiwa <shiraiwa@princeton.edu>
2020-11-23 11:58:12 -0500

    added travis test for cuda and parallel

---
+++ travis/add_cuda.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+set -ev
+
+wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2004/x86_64/cuda-ubuntu2004.pin
+sudo mv cuda-ubuntu2004.pin /etc/apt/preferences.d/cuda-repository-pin-600
+wget https://developer.download.nvidia.com/compute/cuda/11.1.1/local_installers/cuda-repo-ubuntu2004-11-1-local_11.1.1-455.32.00-1_amd64.deb
+sudo dpkg -i cuda-repo-ubuntu2004-11-1-local_11.1.1-455.32.00-1_amd64.deb
+sudo apt-key add /var/cuda-repo-ubuntu2004-11-1-local/7fa2af80.pub
+sudo apt-get update
+sudo apt-get -y install cuda
+
