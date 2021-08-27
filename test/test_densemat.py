4fb9c09e3790720f6b1959d297869c2428179362
Syun'ichi Shiraiwa <shiraiwa@princeton.edu>
2021-08-27 13:38:38 -0400

    fixed memory leak

--- test/test_densemat.py
+++ test/test_densemat.py
@@ -20,6 +20,12 @@ def run_test():
     m.Assign(np.arange(9.).reshape(3,3))
     m.Print()
     print(np.arange(9.).reshape(3,3))
+
+    m = mfem.DenseMatrix(3,5)
+    m.Assign(np.arange(15.).reshape(3,5))
+    m.Print()
+    print(np.arange(15.).reshape(3,5))
+    
     
     x = np.zeros(5)+1
     y = np.zeros(5)+2
@@ -30,6 +36,41 @@ def run_test():
     mfem.Vector(mm.GetData(), 15).Print()
 
     mm.Print("densmat.dat")
+
+    ### added this check based on the issue 99.
+    import tracemalloc
+    import gc
+    tracemalloc.start()
+    n = 4 * 1024 // 8
+    d = 1024
+    K = mfem.DenseMatrix(d,d)
+    for i in range(10): 
+        K.Assign(np.diag(d * np.ones(d)))
+        data = K.GetDataArray()
+        size, peak = tracemalloc.get_traced_memory()        
+        #print(f"{size=}, {peak=}")
+
+    m = mfem.DenseTensor(2, 3, 5)
+    m.Assign(0)
+    m[1, 2, 3] = 5
+    print(m[1, 2, 3])
+    
+    assert np.all(m[3].GetDataArray() == m.GetDataArray()[3]), "First column slicing does not agree"
+
+    t = np.arange(30.).reshape(2, 3, 5)
+    size, peak = tracemalloc.get_traced_memory()        
+    print(f"{size=}, {peak=}")
+    
+    m.Assign(t)
+    data = []
+    for i in range(m.SizeI()):
+       for j in range(m.SizeJ()):
+        for k in range(m.SizeK()):
+            assert m[i, j, k] == t[i, j, k], "Tensor assigne fails"
+
+    # these two should not agree (internal memory layout differs)        
+    print(t.flatten())
+    print(mfem.Vector(m.Data(), 30).GetDataArray())
     
 if __name__=='__main__':
     run_test()
