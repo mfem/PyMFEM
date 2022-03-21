f8015a443e216dc68c1ceb045da51ef92d671079
Syun'ichi Shiraiwa <shiraiwa@princeton.edu>
2022-03-21 19:33:26 -0400

    update libceed to 0.10.0

--- setup.py
+++ setup.py
@@ -42,7 +42,7 @@ except ImportError:
 repo_releases = {  # "mfem": "https://github.com/mfem/mfem/archive/v4.3.tar.gz",
     "metis": "http://glaros.dtc.umn.edu/gkhome/fetch/sw/metis/metis-5.1.0.tar.gz",
     "hypre": "https://github.com/hypre-space/hypre/archive/v2.24.0.tar.gz",
-    "libceed": "https://github.com/CEED/libCEED/archive/refs/tags/v0.9.0.tar.gz",
+    "libceed": "https://github.com/CEED/libCEED/archive/refs/tags/v0.10.0.tar.gz",
     "gslib": "https://github.com/Nek5000/gslib/archive/refs/tags/v1.0.7.tar.gz"}
 
 repos = {"mfem": "https://github.com/mfem/mfem.git",
@@ -1363,8 +1363,8 @@ class BuildPy(_build_py):
                 download('hypre')
                 cmake_make_hypre()
             if build_libceed:
-                #download('libceed')
-                gitclone('libceed',branch='main')
+                download('libceed')
+                #gitclone('libceed',branch='main')
                 make_libceed()
             if build_gslib:
                 download('gslib')
