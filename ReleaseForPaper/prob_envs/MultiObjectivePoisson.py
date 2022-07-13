e2bf3a587d18b43e505ef0593ac338962aee3c1d
JenniferZvonek <jezvonek@gmail.com>
2022-07-13 10:07:21 -0400

    MO, h-refinement version of example 2c; also added new environment class for MO Poisson with specified angle.

--- ReleaseForPaper/prob_envs/MultiObjectivePoisson.py
+++ ReleaseForPaper/prob_envs/MultiObjectivePoisson.py
@@ -82,10 +82,63 @@ class MultiObjPoisson(Poisson):
             return super().GetObservation(self)
 
     def reset(self):
+        # choose new random alpha if alpha is included as an observation
         if self.optimization_type == 'multi_objective':
             if self.observe_alpha == True: 
                 # set random alpha value
-                self.alpha = np.random.uniform(low = 0, high = 1)
+                self.alpha = np.random.uniform(low = 0.4, high = 0.6)
         
         return super().reset()
 
+class Angle_MultiObjPoisson(MultiObjPoisson):
+
+    def __init__(self,**kwargs):
+        super().__init__(**kwargs)  
+        problem_type = kwargs.get('problem_type','lshaped')
+        if (problem_type == 'lshaped'):
+            delattr(self, 'BC')
+            self.BC = mfem.NumbaFunction(ReentrantCornerExact, 2, True).GenerateCoefficient()
+        elif (problem_type == 'wavefront'):
+            print("*** Should not be using RobustAnglePoisson class with wavefront problem - exiting ***")
+            exit()
+        else:
+            delattr(self, 'BC')
+            self.BC = mfem.ConstantCoefficient(0.0)
+        self.num_unif_ref = kwargs.get('num_unif_ref',1)
+        self.Lshapedmeshfile = expanduser(join(os.path.dirname(__file__), '../..', 'data', 'l-shape-benchmark.mesh'))
+        self.circlemeshfile = expanduser(join(os.path.dirname(__file__), '../..', 'data', 'circle_3_4.mesh'))
+
+        self.angle = kwargs.get('angle_lower', np.pi * 0.5)
+        self.angle_lower = kwargs.get('angle_lower', np.pi * 0.25)
+        self.angle_upper = kwargs.get('angle_upper', np.pi * 0.75)
+        if (problem_type == 'lshaped'):
+            self.set_angle(self.angle)
+
+    def set_angle(self, angle):
+        # print("Setting env angle to ", self.angle, flush=True)
+        self.angle = angle
+        self.BC.SetTime(self.angle)
+
+        if self.mesh_name =='l-shape-benchmark.mesh':
+            self.initial_mesh = ReentrantCornerMesh(self.angle, self.Lshapedmeshfile)
+        elif self.mesh_name =='circle_3_4.mesh':
+            self.initial_mesh = ReentrantCornerMesh(self.angle, self.circlemeshfile)
+        else:
+            print("Something went wrong - see set_angle routine; exiting")
+            exit()
+        for _ in range(self.num_unif_ref):
+            self.initial_mesh.UniformRefinement()
+        self.initial_mesh.EnsureNCMesh()
+
+    def reset(self, random_angle=True):
+        # choose new random alpha if alpha is included as an observation
+        if self.optimization_type == 'multi_objective':
+            if self.observe_alpha == True: 
+                # set random alpha value
+                self.alpha = np.random.uniform(low = 0.4, high = 0.6)
+
+        if random_angle:
+            angle = np.random.uniform(self.angle_lower, self.angle_upper, 1).item()
+            # print("Resetting env angle to ", angle)
+            self.set_angle(angle)
+        return super().reset()
