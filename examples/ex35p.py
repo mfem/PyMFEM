8135e1947ed062778b8b3540ad2ca9cd1cf73b9c
Syun'ichi Shiraiwa <shiraiwa@princeton.edu>
2024-01-06 17:35:49 -0500

    ex35p in progress....

--- examples/ex35p.py
+++ examples/ex35p.py
@@ -36,10 +36,52 @@ def run(mesh_file="",
         mu=1.0,
         device='cpu',
         pa=False,
-        freq=-1.0,
-        numba=False):
+        port_bc_attr = None,
+        freq=-1.0,):
 
+    mesh_file = "../data/fichera-mixed.mesh"
+    if not mixed or pa:
+        mesh_file = "../data/fichera.mesh"
+        
+    if a_coef != 0.0:
+         mu_ = 1.0 / a_coef
+    omega = 2*pi
+    if freq > 0.0:
+        omega = 2.0 * pi * freq
 
+    if (len(port_bc_attr) == 0 and
+        (mesh_file == "../data/fichera-mixed.mesh" or
+         mesh_file == "../data/fichera.mesh")):
+        port_bc_attr = mfem.intArray([7, 8, 11, 12])
+
+    conv = mfem.ComplexOperator.HERMITIAN if herm_conv else mfem.ComplexOperator.BLOCK_SYMMETRIC        
+    # 3. Enable hardware devices such as GPUs, and programming models such as
+    #    CUDA, OCCA, RAJA and OpenMP based on command line options.
+    device = mfem.Device(device)
+    if myid == 0:
+        device.Print()
+
+    # 4. Read the mesh from the given mesh file. We can handle triangular,
+    #    quadrilateral, tetrahedral, hexahedral, surface and volume meshes
+    #    with the same code.
+    mesh = mfem.Mesh(mesh_file, 1, 1)
+    dim = mesh.Dimension()
+
+    # 5. Refine the serial mesh on all processors to increase the resolution.    
+    for i in range(ser_ref_levels):
+        pmesh.UniformRefinement()
+
+    # 6a. Define a parallel mesh by a partitioning of the serial mesh. Refine
+    #    this mesh further in parallel to increase the resolution. Once the
+    #    parallel mesh is defined, the serial mesh can be deleted.
+    pmesh = mfem.ParMesh(MPI.COMM_WORLD, mesh)
+    for i in range(par_ref_levels):
+        pmesh.UniformRefinement()
+    
+    # 6b. Extract a submesh covering a portion of the boundary
+    pmesh_cond = mfem.ParrSubMesh.CreateFromBoundary(pmesh, port_bc_attr)
+
+        
 if __name__ == "__main__":
     from mfem.common.arg_parser import ArgParser
 
@@ -118,12 +160,14 @@ if __name__ == "__main__":
     herm = False if args.no_hermitian else True
     args.hermitian = herm
     args.no_hermitian = not herm
+    
     if myid == 0:
         parser.print_options(args)
 
     meshfile = expanduser(
         join(os.path.dirname(__file__), '..', 'data', args.mesh))
 
+    port_bc_attr = [int(x) for x in args.port_bc_attr.split(' ')]
     globals()["mixed"] = not args.hex_mesh
     
     run(mesh_file=meshfile,
@@ -141,5 +185,6 @@ if __name__ == "__main__":
         mu=args.permeability,
         device=args.device,
         pa=args.partial_assembly,
+        port_bc_attr=port_bc_attr,
         freq=args.frequency)
 
