4682fc56976afb2a5bf1bc250077554bd99c2ae2
shiraiwa <shiraiwa@psfc.mit.edu>
2026-02-21 14:59:43 -0800

    avoid linking mfem-common, fixed build_config for missing prefix argument

--- mfem/_par/setup.py
+++ mfem/_par/setup.py
@@ -71,8 +71,8 @@ def get_extensions():
         mpiinc = ''
 
     libraries = ['mfem',]
-    if build_miniapps !=  '0':
-        libraries.append("mfem-common")
+    #if build_miniapps !=  '0':
+    #    libraries.append("mfem-common")
 
     # remove current directory from path
     print("__file__", os.path.abspath(__file__))
